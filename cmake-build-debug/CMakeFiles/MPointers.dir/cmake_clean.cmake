97eda1be3cc1af64af732d7755fd94c803f443f0
Jonathana1196 <jonathana1196@gmail.com>
2018-09-19 22:10:30 -0600

    Agregando archivos iniciales.

---
+++ cmake-build-debug/CMakeFiles/MPointers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MPointers.dir/main.cpp.o"
+  "MPointers.pdb"
+  "MPointers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MPointers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
